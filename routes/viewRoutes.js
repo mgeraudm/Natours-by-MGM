8e2d5049e6656574f1405b195ee9d3390c1d30cb
mgeraudm <mgeraudm@gmail.com>
2022-07-16 21:51:33 -0500

    added stripe alert

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewsController');
 const authController = require('./../controllers/authController');
-const bookingController = require('./../controllers/bookingController');
+//const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
@@ -12,9 +12,10 @@ router.use(viewsController.alerts);
 
 router.get(
   '/',
-  bookingController.createBookingCheckout,
+  //bookingController.createBookingCheckout,
   viewsController.getOverview
 );
+
 router.get(
   '/tour/:slug',
   //authController.protect,
